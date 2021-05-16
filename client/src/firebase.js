6c1876a945d326b614ef67bb2822ebbe456ae745
volkanGoksu <volkan.goksu@outlook.com>
2021-05-16 08:01:14 +0300

    npm firebase ve config

---
+++ client/src/firebase.js
@@ -0,0 +1,13 @@
+
+import * as firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyDzq0_jdHYTnseAMV80L1kJhGFFDlkCXog",
+    authDomain: "react-contact-cc22b.firebaseapp.com",
+    databaseURL: "https://react-contact-cc22b-default-rtdb.firebaseio.com",
+    projectId: "react-contact-cc22b",
+    storageBucket: "react-contact-cc22b.appspot.com",
+    messagingSenderId: "799311948479",
+    appId: "1:799311948479:web:f0557cdb9941511a71340a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
