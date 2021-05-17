2737bb5cb820258df9cf065c4406f3868b4f720b
volkanGoksu <volkan.goksu@outlook.com>
2021-05-17 12:42:37 +0300

    input içindeki değerlere erişe

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,27 +1,48 @@
-import React from 'react'
+
+import React,{useState} from 'react'
 
 export default function ContactForm() { 
+    const formFields = { 
+        name:'',
+        surname:'',
+        number:'',
+        mail:''
+    }
+    const [fields, Setfields] = useState(formFields)
+    const changeFields=(e)=>{
+          var fieldName = e.target.name;
+          var fieldValue = e.target.value
+
+          Setfields({
+              ...fields,[fieldName]:fieldValue
+          })
+    }
+    const dataSave=(e)=>{
+         e.preventDefault();
+        console.log(fields);
+    }
+  
     return (
-        <form>
+        <form onSubmit={dataSave}>
             <div className="form-group input-group">
                <div>                
-                     < input className='form-control' type='text' placeholder='Name' ></input>
+                     < input className='form-control' onChange={changeFields} type='text' name='name' placeholder='Name' ></input>
                 </div>
             </div>
             <div className="form-group input-group">
                 <div className="input-group-prepend">
-                     < input className='form-control' placeholder='Surname'></input>
+                     < input className='form-control' onChange={changeFields} name='surname' placeholder='Surname'></input>
                 </div>
             </div>
             <div className = "form-row">
             <div className="form-group input-group">
                 <div className="input-group-prepend">
-                     < input className='form-control' placeholder='Number' ></input>
+                     < input className='form-control' onChange={changeFields}  name='number' placeholder='Number' ></input>
                 </div>
             </div>
             <div className="form-group input-group ">
                 <div className="input-group-prepend">
-                     < input className='form-control' placeholder='Email' ></input>
+                     < input className='form-control' onChange={changeFields} name='mail' placeholder='Email' ></input>
                 </div>
             </div>
             </div>
