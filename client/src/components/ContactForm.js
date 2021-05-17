fc5e91f55e88a8894ac24aee223eb4551af535ab
volkanGoksu <volkan.goksu@outlook.com>
2021-05-17 12:30:38 +0300

    Contactform html

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
-export default function ContactForm() {
+export default function ContactForm() { 
     return (
-        <div>
-            İletişim Formları gelecek
-        </div>
+        <form>
+            <div className="form-group input-group">
+               <div>                
+                     < input className='form-control' type='text' placeholder='Name' ></input>
+                </div>
+            </div>
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                     < input className='form-control' placeholder='Surname'></input>
+                </div>
+            </div>
+            <div className = "form-row">
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                     < input className='form-control' placeholder='Number' ></input>
+                </div>
+            </div>
+            <div className="form-group input-group ">
+                <div className="input-group-prepend">
+                     < input className='form-control' placeholder='Email' ></input>
+                </div>
+            </div>
+            </div>
+            <div className="form-group">
+                <input type = "submit" value="kaydet" className = "btn btn-success"/>
+            </div>
+        </form>
     )
 }
